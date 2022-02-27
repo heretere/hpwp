960df0f8c4710ce1f1316587a80ce16d51a16168
Justin <justin@cargochief.com>
2022-02-27 11:58:32 -0500

    v1.5.4

--- src/main/java/com/heretere/hpwp/PerWorldPlugins.java
+++ src/main/java/com/heretere/hpwp/PerWorldPlugins.java
@@ -65,11 +65,6 @@ import com.heretere.hpwp.update.UpdaterRunnable;
     defaultValue = PermissionDefault.FALSE
 )
 @Permission(
-    name = "hpwp.bypass.commands",
-    desc = "Execute all commands even for disabled plugins",
-    defaultValue = PermissionDefault.FALSE
-)
-@Permission(
     name = "hpwp.*",
     desc = "Wildcard hpwp permission",
     defaultValue = PermissionDefault.OP,
@@ -88,7 +83,6 @@ import com.heretere.hpwp.update.UpdaterRunnable;
     desc = "Wildcard hpwp bypass permission",
     defaultValue = PermissionDefault.FALSE,
     children = {
-        @ChildPermission(name = "hpwp.bypass.commands"),
         @ChildPermission(name = "hpwp.bypass.chat")
     }
 )
